339e4d8eba9a71c22f984730a2bc3069dfefff27
Don Eisele <ksuquix@gmail.com>
2013-07-20 19:40:30 -0500

    add bricklink.js

---
+++ bricklink.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name           Bricklink Tweaks
+// @namespace      https://github.com/ksuquix/userscript-tweaks
+// @version        0.0.1
+// @description    Add tweaks / features to bricklink
+// @include        http://www.bricklink.com/*
+// @require        http://code.jquery.com/jquery-1.10.2.min.js
+// @run-at         document-end
+// ==/UserScript==
+
+
