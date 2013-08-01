6b67b0e09b91fb22875edf30133dac41285789fc
Don Eisele <ksuquix@gmail.com>
2013-07-31 22:22:33 -0500

    doing with pre-hook instead of filters

---
+++ test.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           Bricklink Tweaks
+// @namespace      https://github.com/ksuquix/userscript-tweaks
+// @version        0.fixme
+// @description    Add tweaks / features to bricklink
+// @include        http://www.bricklink.com/*
+// @require        http://code.jquery.com/jquery-1.10.2.min.js
+// @run-at         document-end
+// ==/UserScript==
+
