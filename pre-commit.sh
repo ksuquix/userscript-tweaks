4da67cce8a19ec109d541e3d10cf1394bd86cbc0
Don Eisele <ksuquix@gmail.com>
2013-07-31 22:53:20 -0500

    remove test file, add stash push/pop to precommit

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,11 +3,17 @@
 dir=`dirname $0`
 ver=`$dir/../../git-revision.sh -c -p`
 
+git stash -q --keep-index
+
 # for a pre-commit hook, use --cached instead of HEAD^ HEAD
 IFS=$'\n'
 git diff --name-only --cached | grep '\.js$' |
 while read file; do
+  if [ -f "$file" ]; then
     echo versioning $file
     # exit immediately if the script fails
     echo -e '/^\/\/[ \t]*@version\ns/version\([ \t]*\).*/version\1'$ver'\nw\nq\n' | ed $file
+  fi
 done
+
+git stash pop -q
