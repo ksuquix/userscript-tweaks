1d67a040d5c3105579610a0122dbe2bbcff86696
ksuquix <ksuquix@gmail.com>
2014-06-29 23:43:06 -0500

    raw.github blocking direct content

--- bricklink.tamper.js
+++ bricklink.tamper.js
@@ -15,9 +15,12 @@
 //$('title').after('<script type="text/javascript" language="javascript" src="https://raw.github.com/ksuquix/userscript-tweaks/master/bricklink-subinclude.js"></script>');
 var oHead = document.getElementsByTagName('HEAD').item(0);
 var oScript= document.createElement("script");
-oScript.type = "text/javascript";
-oScript.src="https://raw.github.com/ksuquix/userscript-tweaks/master/bricklink-subinclude.js";
-oHead.appendChild( oScript);
+
+oScript.innerHTML('<script type="text/javascript">'+"https://raw.github.com/ksuquix/userscript-tweaks/master/bricklink-subinclude.js"+'</script>')
+
+// oScript.type = "text/javascript";
+// oScript.src="https://raw.github.com/ksuquix/userscript-tweaks/master/bricklink-subinclude.js";
+// oHead.appendChild( oScript);
 
 
 // On Part catalog entry, add an "(Add)" link next to price guide that goes into inventory add dialog with
