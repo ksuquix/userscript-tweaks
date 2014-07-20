4bfe5366bbab3b35809b0816aa010fff22741918
Don Eisele <ksuquix@gmail.com>
2014-07-19 20:35:29 -0500

    basebrick initial

--- bricklink.tamper.js
+++ bricklink.tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Bricklink Tweaks
 // @namespace      https://github.com/ksuquix/userscript-tweaks
-// @version        0.1.39
+// @version        0.1.310
 // @description    Add tweaks / features to bricklink
 // @include        http://www.bricklink.com/*
 // @require        http://code.jquery.com/jquery-1.10.2.min.js
@@ -54,6 +54,13 @@ if(window.location.pathname.indexOf('catalogItem.asp')>0 || window.location.path
 	    replace(/colorID=\d*/,'');
 	$(this).after('&nbsp;<a href="'+url+'">(Add)</a>&nbsp;<a href="'+burl+'">(BB)</a><!-- 4 -->');
     });	
+    $('img[src*=http://img.bricklink.com/P/]').each(function() {
+	url = $(this).attr('src').
+	    replace(/.*P\//,'').
+	    replace(/\..*/,'').
+	    repalce(/^/,'inventory_add.asp?a=p&itemID=').
+	    replace(/\//,'&colorID=');
+	$(this).after('&nbsp;<a href="'+url+'">(AddC)</a><!-- 6 -->');
 }
 
 // If inventory page, automatically trigger the price guide ajax pull
